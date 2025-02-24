beb0aff6569ca2f6656c765077cba68d69386453
Brian Tiemann <btman@mac.com>
2025-02-23 21:36:19 -0500

    Add VLAN Translation Policy to bulk edit forms and tables for Interface and VMInterface

--- netbox/virtualization/tables/virtualmachines.py
+++ netbox/virtualization/tables/virtualmachines.py
@@ -122,7 +122,7 @@ class VMInterfaceTable(BaseInterfaceTable):
         fields = (
             'pk', 'id', 'name', 'virtual_machine', 'enabled', 'mtu', 'mode', 'description', 'tags', 'vrf',
             'primary_mac_address', 'l2vpn', 'tunnel', 'ip_addresses', 'fhrp_groups', 'untagged_vlan', 'tagged_vlans',
-            'qinq_svlan', 'created', 'last_updated',
+            'qinq_svlan', 'created', 'last_updated', 'vlan_translation_policy',
         )
         default_columns = ('pk', 'name', 'virtual_machine', 'enabled', 'description')
 
