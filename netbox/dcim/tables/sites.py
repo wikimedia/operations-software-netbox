d83c2f45bc473cae23666c548d54309d924d26a7
Renato Almeida de Oliveira Zaroubin <renato.almeida.oliveira@gmail.com>
2025-03-04 18:34:23 +0000

    Add vlangroup into LocationTable, LocationListView queryset and related_models in LocationView

--- netbox/dcim/tables/sites.py
+++ netbox/dcim/tables/sites.py
@@ -146,6 +146,11 @@ class LocationTable(TenancyColumnsMixin, ContactsColumnMixin, NetBoxTable):
         url_params={'location_id': 'pk'},
         verbose_name=_('Devices')
     )
+    vlangroup_count = columns.LinkedCountColumn(
+        viewname='ipam:vlangroup_list',
+        url_params={'location': 'pk'},
+        verbose_name=_('VLAN Groups')
+    )
     tags = columns.TagColumn(
         url_name='dcim:location_list'
     )
@@ -157,8 +162,9 @@ class LocationTable(TenancyColumnsMixin, ContactsColumnMixin, NetBoxTable):
         model = Location
         fields = (
             'pk', 'id', 'name', 'site', 'status', 'facility', 'tenant', 'tenant_group', 'rack_count', 'device_count',
-            'description', 'slug', 'contacts', 'tags', 'actions', 'created', 'last_updated',
+            'description', 'slug', 'contacts', 'tags', 'actions', 'created', 'last_updated', 'vlangroup_count',
         )
         default_columns = (
-            'pk', 'name', 'site', 'status', 'facility', 'tenant', 'rack_count', 'device_count', 'description'
+            'pk', 'name', 'site', 'status', 'facility', 'tenant', 'rack_count', 'device_count', 'vlangroup_count',
+            'description'
         )
