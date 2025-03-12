749a83d7427b7ddc911eed68cdcb931f34f8efa7
Renato Almeida de Oliveira <renato.almeida.oliveira@gmail.com>
2025-03-12 14:00:40 -0300

    Change JSONField, prepare_value to output str with enclosing " (#18846)

--- netbox/utilities/forms/fields/fields.py
+++ netbox/utilities/forms/fields/fields.py
@@ -111,7 +111,7 @@ class JSONField(_JSONField):
             try:
                 value = json.loads(value, cls=self.decoder)
             except json.decoder.JSONDecodeError:
-                return value
+                return f'"{value}"'
         return json.dumps(value, sort_keys=True, indent=4, ensure_ascii=False, cls=self.encoder)
 
 
