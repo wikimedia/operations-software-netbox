00947036093565536d5cf03b71b530764410dfde
Benjamin Dale <ben.dale@gmail.com>
2025-03-18 00:39:28 +1000

    Closes #18816:Add FC/UPC, FC/APC and FC/PC port types (#18913)
    * Closes #18816:Add FC/UPC, FC/APC and FC/PC port types
    
    * Remove release note
    
    ---------
    
    Co-authored-by: Ben Dale <7621213-ben_dale@users.noreply.gitlab.com>
    Co-authored-by: Jeremy Stretch <jstretch@netboxlabs.com>

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -1345,6 +1345,9 @@ class PortTypeChoices(ChoiceSet):
     TYPE_SC_UPC = 'sc-upc'
     TYPE_SC_APC = 'sc-apc'
     TYPE_FC = 'fc'
+    TYPE_FC_PC = 'fc-pc'
+    TYPE_FC_UPC = 'fc-upc'
+    TYPE_FC_APC = 'fc-apc'
     TYPE_LC = 'lc'
     TYPE_LC_PC = 'lc-pc'
     TYPE_LC_UPC = 'lc-upc'
@@ -1405,6 +1408,9 @@ class PortTypeChoices(ChoiceSet):
             _('Fiber Optic'),
             (
                 (TYPE_FC, 'FC'),
+                (TYPE_FC_PC, 'FC/PC'),
+                (TYPE_FC_UPC, 'FC/UPC'),
+                (TYPE_FC_APC, 'FC/APC'),
                 (TYPE_LC, 'LC'),
                 (TYPE_LC_PC, 'LC/PC'),
                 (TYPE_LC_UPC, 'LC/UPC'),
