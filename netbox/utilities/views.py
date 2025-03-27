7d80a45bf8462cad60d052460b8e3044d4e005ff
Renato Almeida de Oliveira <renato.almeida.oliveira@gmail.com>
2025-03-27 09:33:09 -0300

    Fixes: #16144 GetReturnURLMixin Support for Plugin Views (#18996)
    * Add plugin support to GetReturnURLMixin
    
    * use get_viewname instead of resolving the name

--- netbox/utilities/views.py
+++ netbox/utilities/views.py
@@ -149,9 +149,8 @@ class GetReturnURLMixin:
 
         # Attempt to dynamically resolve the list view for the object
         if hasattr(self, 'queryset'):
-            model_opts = self.queryset.model._meta
             try:
-                return reverse(f'{model_opts.app_label}:{model_opts.model_name}_list')
+                return reverse(get_viewname(self.queryset.model, 'list'))
             except NoReverseMatch:
                 pass
 
