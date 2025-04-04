621b29cd71839811f0f3f75639523205087e4789
Jeremy Stretch <jstretch@netboxlabs.com>
2025-04-04 12:18:42 -0400

    Closes #19035: Move the registration of core event types to the app config (#19088)

--- netbox/core/events.py
+++ netbox/core/events.py
@@ -1,7 +1,3 @@
-from django.utils.translation import gettext as _
-
-from netbox.events import EventType, EVENT_TYPE_KIND_DANGER, EVENT_TYPE_KIND_SUCCESS, EVENT_TYPE_KIND_WARNING
-
 __all__ = (
     'JOB_COMPLETED',
     'JOB_ERRORED',
@@ -22,12 +18,3 @@ JOB_STARTED = 'job_started'
 JOB_COMPLETED = 'job_completed'
 JOB_FAILED = 'job_failed'
 JOB_ERRORED = 'job_errored'
-
-# Register core events
-EventType(OBJECT_CREATED, _('Object created')).register()
-EventType(OBJECT_UPDATED, _('Object updated')).register()
-EventType(OBJECT_DELETED, _('Object deleted'), destructive=True).register()
-EventType(JOB_STARTED, _('Job started')).register()
-EventType(JOB_COMPLETED, _('Job completed'), kind=EVENT_TYPE_KIND_SUCCESS).register()
-EventType(JOB_FAILED, _('Job failed'), kind=EVENT_TYPE_KIND_WARNING).register()
-EventType(JOB_ERRORED, _('Job errored'), kind=EVENT_TYPE_KIND_DANGER).register()
