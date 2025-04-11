6ed41f66802dd44bbf0ca79b9adbd5be09268d02
Jeremy Stretch <jstretch@netboxlabs.com>
2025-04-11 08:20:24 -0400

    Fixes #18955: Include the "clear" button only for non-required dropdown fields (#18971)
    * Include the 'clear' button only for non-required fields
    
    * Apply the 'required' attribute for mandatory <select> fields
    
    * Recompile static assets

--- netbox/project-static/src/select/config.ts
+++ netbox/project-static/src/select/config.ts
@@ -5,11 +5,13 @@ interface PluginConfig {
 export function getPlugins(element: HTMLSelectElement): object {
   const plugins: PluginConfig = {};
 
-  // Enable "clear all" button
-  plugins.clear_button = {
-    html: (data: Dict) =>
-      `<i class="mdi mdi-close-circle ${data.className}" title="${data.title}"></i>`,
-  };
+  // Enable "clear all" button for non-required fields
+  if (!element.required) {
+    plugins.clear_button = {
+      html: (data: Dict) =>
+        `<i class="mdi mdi-close-circle ${data.className}" title="${data.title}"></i>`,
+    };
+  }
 
   // Enable individual "remove" buttons for items on multi-select fields
   if (element.hasAttribute('multiple')) {
